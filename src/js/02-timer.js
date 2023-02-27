5fe00a89d775bdd2252033dcbf3ad93b67844c79
Maksym Vasiliev <makskremen74@gmail.com>
2023-02-27 11:04:59 +0200

    end tasks

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+
+import flatpickr from 'flatpickr';
+import convertMs from './dateConvert';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+const imputDatePickerRef = document.querySelector('#datetime-picker');
+const btnStartRef = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+
+let timeDifference = 0;
+let timerId = null;
+let formatDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    currentDifferenceDate(selectedDates[0]);
+  },
+};
+
+btnStartRef.setAttribute('disabled', true);
+
+
+flatpickr(imputDatePickerRef, options);
+
+btnStartRef.addEventListener('click', onBtnStart);
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && timerId) {
+    clearInterval(timerId);
+
+    imputDatePickerRef.removeAttribute('disabled');
+    btnStartRef.setAttribute('disabled', true);
+
+    secondsRef.textContent = '00';
+    minutesRef.textContent = '00';
+    hoursRef.textContent = '00';
+    daysRef.textContent = '00';
+  }
+});
+
+function onBtnStart() {
+  timerId = setInterval(startTimer, 1000);
+}
+function currentDifferenceDate(selectedDates) {
+  const currentDate = Date.now();
+
+  if (selectedDates < currentDate) {
+    btnStartRef.setAttribute('disabled', true);
+    return Notify.failure('Please choose a date in the future');
+  }
+
+  timeDifference = selectedDates.getTime() - currentDate;
+  formatDate = convertMs(timeDifference);
+
+  renderDate(formatDate);
+  btnStartRef.removeAttribute('disabled');
+}
+
+function startTimer() {
+  btnStartRef.setAttribute('disabled', true);
+  imputDatePickerRef.setAttribute('disabled', true);
+
+  timeDifference -= 1000;
+
+  if (secondsRef.textContent <= 0 && minutesRef.textContent <= 0) {
+    Notify.success('Time end');
+    clearInterval(timerId);
+  } else {
+    formatDate = convertMs(timeDifference);
+    renderDate(formatDate);
+  }
+}
+
+function renderDate(formatDate) {
+  secondsRef.textContent = formatDate.seconds;
+  minutesRef.textContent = formatDate.minutes;
+  hoursRef.textContent = formatDate.hours;
+  daysRef.textContent = formatDate.days;
+}
